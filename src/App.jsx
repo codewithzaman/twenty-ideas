17c413de7b77fe50ba423ed20781465c27f556e2
codewithzaman <naderzaman50@gmail.com>
2024-05-04 22:28:56 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,20 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    
+    <div data-theme="bumblebee">
       <h1 className="text-5xl font-bold underline">
       Hello world!
+      <br />
+      <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-link">Link</button>
     </h1>
-    </>
+    </div>
   )
 }
 
